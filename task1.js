2c58ddf173b8100f06ba04d57e9767ab8a3a55fd
shrutinimai <nimaishruti@gmail.com>
2025-01-19 23:56:08 +0530

    task1 prblm completed

---
+++ task1.js
@@ -0,0 +1,10 @@
+const heading=document.getElementById("main-heading")
+heading.textContent="fruit_world";
+heading.style.color="orange";
+heading.style.backgroundColor="green";
+const header=document.getElementById("header");
+header.style.borderBottom="3px solid orange";
+const basketheading=document.getElementById("basket-heading");
+basketheading.style.color="green";
+const thanksgiving=document.getElementById("thanks");
+thanksgiving.innerHTML="<p>thanks for visisting us!</p>"
