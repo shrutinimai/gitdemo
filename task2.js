a78bc5e9c8c7189125f1161ebf19d774e72ecb16
shrutinimai <nimaishruti@gmail.com>
2025-01-20 00:41:47 +0530

    2nd task uploaded

---
+++ task2.js
@@ -0,0 +1,7 @@
+const fruits=document.getElementsByClassName("fruits");
+for(let i=0;i<fruits.length;i++){
+    fruits[2].style.backgroundColor="yellow";
+}
+for(let i=0;i<fruits.length;i++){
+    fruits[i].style.fontWeight="bold";
+}
